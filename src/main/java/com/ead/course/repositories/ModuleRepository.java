46fa3f5d7ffadcd8ce4f965cf2bc989fa5463e64
gfjgabriel <gfjgabriel@gmail.com>
2024-02-12 17:19:18 -0300

    Manual cascade delete

--- src/main/java/com/ead/course/repositories/ModuleRepository.java
+++ src/main/java/com/ead/course/repositories/ModuleRepository.java
@@ -3,7 +3,10 @@ package com.ead.course.repositories;
 import com.ead.course.models.ModuleModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ModuleRepository extends JpaRepository<ModuleModel, UUID> {
+
+    List<ModuleModel> findAllByCourse_courseId(UUID uuid);
 }
