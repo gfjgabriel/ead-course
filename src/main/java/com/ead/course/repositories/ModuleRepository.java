18674f2090ffb7486433b22dcedf71d47877343a
gfjgabriel <gfjgabriel@gmail.com>
2024-02-13 21:33:07 -0300

    Module controller

--- src/main/java/com/ead/course/repositories/ModuleRepository.java
+++ src/main/java/com/ead/course/repositories/ModuleRepository.java
@@ -4,9 +4,12 @@ import com.ead.course.models.ModuleModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ModuleRepository extends JpaRepository<ModuleModel, UUID> {
 
     List<ModuleModel> findAllByCourse_courseId(UUID uuid);
+
+    Optional<ModuleModel> findByCourse_courseIdAndModuleId(UUID courseId, UUID moduleId);
 }
