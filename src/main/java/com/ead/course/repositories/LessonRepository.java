e24584f3b9b6b972d2c83833d30856ee79f832f7
gfjgabriel <gfjgabriel@gmail.com>
2024-02-13 21:52:47 -0300

    LessonController

--- src/main/java/com/ead/course/repositories/LessonRepository.java
+++ src/main/java/com/ead/course/repositories/LessonRepository.java
@@ -4,9 +4,12 @@ import com.ead.course.models.LessonModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface LessonRepository extends JpaRepository<LessonModel, UUID> {
 
     List<LessonModel> findAllByModule_ModuleId(UUID moduleId);
+
+    Optional<LessonModel> findByModule_ModuleIdAndLessonId(UUID moduleId, UUID lessonId);
 }
--- src/main/java/com/ead/course/services/ModuleService.java
+++ src/main/java/com/ead/course/services/ModuleService.java
@@ -15,4 +15,6 @@ public interface ModuleService {
     Optional<ModuleModel> findModuleByCourse(UUID courseId, UUID moduleId);
 
     List<ModuleModel> findAllByCourse(UUID courseId);
+
+    Optional<ModuleModel> findById(UUID moduleId);
 }
