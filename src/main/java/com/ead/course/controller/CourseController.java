65f7fb88c2ead8dd2441e94ad8c1e482954a975a
gfjgabriel <gfjgabriel@gmail.com>
2024-02-27 21:41:42 -0300

    Delete related userCourseModel when user is deleted

--- src/main/java/com/ead/course/controller/CourseController.java
+++ src/main/java/com/ead/course/controller/CourseController.java
@@ -4,7 +4,9 @@ import com.ead.course.dtos.CourseDto;
 import com.ead.course.models.CourseModel;
 import com.ead.course.services.CourseService;
 import com.ead.course.specifications.SpecificationTemplate;
-import com.ead.course.specifications.SpecificationTemplate.CourseSpec;
+import com.ead.course.validation.CourseValidator;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -13,6 +15,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,26 +30,38 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Log4j2
 @RestController
+@AllArgsConstructor
 @RequestMapping("/courses")
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class CourseController {
 
-    @Autowired
-    CourseService courseService;
+    private final CourseService courseService;
+    private final CourseValidator validator;
 
     @PostMapping
-    public ResponseEntity<Object> saveCourse(@RequestBody @Valid CourseDto courseDto) {
+    public ResponseEntity<Object> saveCourse(@RequestBody CourseDto courseDto, Errors errors) {
+        log.debug("POST saveCourse courseDto received {}", courseDto);
+
+        validator.validate(courseDto, errors);
+        if (errors.hasErrors()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors.getAllErrors());
+        }
+
         var courseModel = new CourseModel();
         BeanUtils.copyProperties(courseDto, courseModel);
         courseModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
         courseModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+        courseService.save(courseModel);
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(courseService.save(courseModel));
+        log.debug("POST saveCourse courseModel saved {}", courseModel);
+        log.info("Course saved successfully courseId {}", courseModel.getCourseId());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(courseModel);
     }
 
     @DeleteMapping("/{courseId}")
--- src/main/java/com/ead/course/repositories/CourseUserRepository.java
+++ src/main/java/com/ead/course/repositories/CourseUserRepository.java
@@ -4,9 +4,12 @@ import com.ead.course.models.CourseModel;
 import com.ead.course.models.CourseUserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CourseUserRepository extends JpaRepository<CourseUserModel, UUID> {
 
     boolean existsByCourseAndUserId(CourseModel courseModel, UUID userId);
+
+    List<CourseUserModel> findAllByCourse_CourseId(UUID courseModelId);
 }
